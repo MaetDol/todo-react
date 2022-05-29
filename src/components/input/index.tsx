47f8189422d8caaf81a2104977e9feb2afe7ed70
MaetDol <rndka9@gmail.com>
2022-05-29 15:00:29 +0900

    feat, Input 컴포넌트 생성

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,3 +1,24 @@
-export default function Input() {
-  return <input type="text" />;
+import { StyledInput } from './styled';
+
+export default function Input({
+  placeholder,
+  setValue,
+  value,
+  type = 'text',
+}: props) {
+  return (
+    <StyledInput
+      type={type}
+      placeholder={placeholder}
+      onChange={(e) => setValue(e.target.value)}
+      value={value}
+    />
+  );
 }
+
+type props = {
+  placeholder?: string;
+  type?: 'text' | 'number';
+  value: string;
+  setValue: (value: string) => void;
+};
