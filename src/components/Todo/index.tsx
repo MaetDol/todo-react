0a7c1a6b7ecc17dc94c748da0e2b67487eb7fb32
MaetDol <rndka9@gmail.com>
2022-05-30 02:39:17 +0900

    feat, Todo 컴포넌트 구현

---
+++ src/components/Todo/index.tsx
@@ -0,0 +1,78 @@
+import Icon, { Icons } from 'components/Icon';
+import { ChangeEvent, KeyboardEvent, useEffect, useRef, useState } from 'react';
+import {
+  StyledDeleteButton,
+  StyledEditButton,
+  StyledInput,
+  StyledStatus,
+  StyledText,
+  StyledWrapper,
+  StylessButton,
+} from './styled';
+
+export default function Todo({
+  checked,
+  setChecked,
+  content,
+  setContent,
+  edit = false,
+}: Props) {
+  const [editing, setEditing] = useState(edit);
+  useEffect(() => {
+    setEditing(edit);
+  }, [edit]);
+
+  const ref = useRef<HTMLInputElement>(null);
+  const [prevContent, setPrevContent] = useState(content);
+  const startEdit = () => {
+    setEditing(true);
+    setPrevContent(content);
+    // display: none 에서 바뀌고 업데이트 되기전엔 focus 되지 않습니다
+    // useLayoutEffect 를 고려해봐야합니다
+    setTimeout(() => {
+      if (ref.current) ref.current.focus();
+    });
+  };
+
+  const onEdit = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') setEditing(false);
+    if (e.key === 'Escape') {
+      setEditing(false);
+      setContent(prevContent);
+    }
+  };
+
+  return (
+    <StyledWrapper>
+      <StylessButton onClick={() => setChecked(!checked)}>
+        <StyledStatus checked={checked} />
+      </StylessButton>
+      <StyledText checked={checked} edit={editing}>
+        {content}
+      </StyledText>
+      <StyledInput
+        ref={ref}
+        edit={editing}
+        onChange={(e) => setContent(e.target.value)}
+        onKeyDown={onEdit}
+        value={content}
+      />
+
+      <StyledEditButton onClick={startEdit} checked={checked}>
+        <Icon icon={Icons.edit} />
+      </StyledEditButton>
+
+      <StyledDeleteButton>
+        <Icon icon={Icons.cancel} />
+      </StyledDeleteButton>
+    </StyledWrapper>
+  );
+}
+
+type Props = {
+  checked: boolean;
+  setChecked: (checked: boolean) => void;
+  content: string;
+  setContent: (content: string) => void;
+  edit?: boolean;
+};
