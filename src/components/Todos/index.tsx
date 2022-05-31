39b62138fb442e0ebe4b9f7f5bdde47808f9bf71
이강민(맷돌) <kangmin98@jaranda.kr>
2022-05-31 23:59:55 +0900

    feat, Todo list 컴포넌트 선언

---
+++ src/components/Todos/index.tsx
@@ -0,0 +1,40 @@
+import Todo from "components/Todo";
+import { TodoType } from "models/Todo";
+import { StyledItem, StyledList } from "./styled";
+
+export default function Todos({todos, setTodos}: Props) {
+
+    const deleteHandler = (id: number) => () =>  {
+        setTodos(todos.filter(todo => todo.id !==  id));
+    }
+
+    const checkHandler = (id:number) =>(checked:boolean) => {
+        const todo  = todos.find(todo => todo.id === id);
+        if(!todo) return;
+        todo.checked = checked;
+        setTodos([...todos]);
+    }
+
+    const contentHandler = (id:number) => (content:  string) => {
+        const todo = todos.find(todo  =>  todo.id ===  id);
+        if(!todo) return
+        todo.content = content;
+        setTodos([...todos]);
+    }
+
+    return (<StyledList>{todos.map(todo  => 
+        <StyledItem key={todo.id}><Todo
+        checked={todo.checked}
+        content={todo.content}
+        deleteHandler={deleteHandler(todo.id)}
+        setChecked={checkHandler(todo.id)}
+        setContent={contentHandler(todo.id)}
+        /></StyledItem>
+    )}
+    </StyledList>);
+}
+
+type Props = {
+    todos: TodoType[]
+    setTodos: (todos: TodoType[]) => void;
+}
---
+++ src/components/Todos/styled.ts
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+export const StyledList = styled.ul`
+  padding: 0;
+  margin: 0;
+`;
+
+export const StyledItem = styled.li`
+  margin-bottom: 3.2rem;
+
+  &:last-child {
+    margin-bottom: 0;
+  }
+`;
