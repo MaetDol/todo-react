db1c318e81a427dd74b39e07ea6510f8f82bf817
MaetDol <rndka9@gmail.com>
2022-06-01 21:42:43 +0900

    feat, 수정 상태를 관리하는 함수 추가

--- src/components/Todos/index.tsx
+++ src/components/Todos/index.tsx
@@ -1,40 +1,55 @@
-import Todo from "components/Todo";
-import { TodoType } from "models/Todo";
-import { StyledItem, StyledList } from "./styled";
+import Todo from 'components/Todo';
+import { TodoType } from 'models/Todo';
+import { StyledItem, StyledList } from './styled';
 
-export default function Todos({todos, setTodos}: Props) {
+export default function Todos({ todos, setTodos }: Props) {
+  const deleteHandler = (id: number) => () => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
 
-    const deleteHandler = (id: number) => () =>  {
-        setTodos(todos.filter(todo => todo.id !==  id));
-    }
+  const checkHandler = (id: number) => (checked: boolean) => {
+    const todo = todos.find((todo) => todo.id === id);
+    if (!todo) return;
+    todo.checked = checked;
+    setTodos([...todos]);
+  };
 
-    const checkHandler = (id:number) =>(checked:boolean) => {
-        const todo  = todos.find(todo => todo.id === id);
-        if(!todo) return;
-        todo.checked = checked;
-        setTodos([...todos]);
-    }
+  const contentHandler = (id: number) => (content: string) => {
+    const todo = todos.find((todo) => todo.id === id);
+    if (!todo) return;
 
-    const contentHandler = (id:number) => (content:  string) => {
-        const todo = todos.find(todo  =>  todo.id ===  id);
-        if(!todo) return
-        todo.content = content;
-        setTodos([...todos]);
-    }
+    todo.content = content;
+    setTodos([...todos]);
+  };
 
-    return (<StyledList>{todos.map(todo  => 
-        <StyledItem key={todo.id}><Todo
-        checked={todo.checked}
-        content={todo.content}
-        deleteHandler={deleteHandler(todo.id)}
-        setChecked={checkHandler(todo.id)}
-        setContent={contentHandler(todo.id)}
-        /></StyledItem>
-    )}
-    </StyledList>);
+  const onEditHandler = (id: number) => (editing: boolean) => {
+    const todo = todos.find((todo) => todo.id === id);
+    if (!todo) return;
+
+    todo.editing = editing;
+    setTodos([...todos]);
+  };
+
+  return (
+    <StyledList>
+      {todos.map((todo) => (
+        <StyledItem key={todo.id}>
+          <Todo
+            checked={todo.checked}
+            content={todo.content}
+            deleteHandler={deleteHandler(todo.id)}
+            setChecked={checkHandler(todo.id)}
+            setContent={contentHandler(todo.id)}
+            edit={todo.editing}
+            setEdit={onEditHandler(todo.id)}
+          />
+        </StyledItem>
+      ))}
+    </StyledList>
+  );
 }
 
 type Props = {
-    todos: TodoType[]
-    setTodos: (todos: TodoType[]) => void;
-}
+  todos: TodoType[];
+  setTodos: (todos: TodoType[]) => void;
+};
