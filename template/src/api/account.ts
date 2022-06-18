5c14ffc5e3f35b5825ff621fb88fd1ff185c4b09
MaetDol <rndka9@gmail.com>
2022-06-18 22:20:08 +0900

    feat, 로그인, 회원가입 훅 껍데기 생성

---
+++ template/src/api/account.ts
@@ -0,0 +1,9 @@
+import { useEffect } from 'react';
+
+export function useSignUp(onSuccess: Function, onFailed: Function) {
+  return (email: string, passsword: string) => {};
+}
+
+export function useSignIn(onSuccess: Function, onFailed: Function) {
+  return (email: string, passsword: string) => {};
+}
