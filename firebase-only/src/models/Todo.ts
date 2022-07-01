c6de5fc325acc1dc78322d09d768196c7487ba6f
MaetDol <rndka9@gmail.com>
2022-07-01 23:44:08 +0900

    firebase 에 저장할 수 있게 객체화 하는 메서드 추가

--- firebase-only/src/models/Todo.ts
+++ firebase-only/src/models/Todo.ts
@@ -1,8 +1,21 @@
 export class Todo {
+  created_at: Date;
+
   constructor(
     public content: string,
     public id: number,
     public checked: boolean,
     public editing: boolean
-  ) {}
+  ) {
+    this.created_at = new Date();
+  }
+
+  serialize() {
+    return {
+      content: this.content,
+      id: this.id,
+      done: this.checked,
+      created_at: this.created_at,
+    };
+  }
 }
