18ee443a984a4132750dc4ac3ff3560c7103d487
MaetDol <rndka9@gmail.com>
2022-07-02 22:26:52 +0900

    파이어베이스 데이터 converter 작성

--- firebase-only/src/api/todo.ts
+++ firebase-only/src/api/todo.ts
@@ -1,8 +1,23 @@
 import { Todo } from 'models/Todo';
 import React, { useEffect, useState } from 'react';
+import { Utils } from 'utils/util';
 import { currentUser } from './firebase';
 import { addDocument } from './firebase/db';
 import { getDatas } from './firebase/db/db';
+
+function fromFirestore(data: any): Todo {
+  if (!data) return new Todo('', false, false);
+
+  const { content, checked, created_at, updated_at } = data;
+  const todo = new Todo(content, checked, false);
+  todo.created_at = created_at
+    ? Utils.fromFirestoreDate(created_at)
+    : new Date();
+  todo.updated_at = updated_at
+    ? Utils.fromFirestoreDate(updated_at)
+    : new Date();
+  return todo;
+}
 
 export function useAddTodo(
   setTodos: React.Dispatch<React.SetStateAction<Todo[]>>
@@ -22,9 +37,10 @@ export function useTodos() {
   useEffect(() => {
     const user = currentUser();
     if (!user) return;
-    if (!user.email) throw new Error(`Cannot found user's email. ${user}`);
 
-    getDatas(user.uid).then(setTodos);
+    getDatas(user.uid).then((datas) => {
+      setTodos(datas.map(fromFirestore));
+    });
   }, []);
 
   return { todos, setTodos };
